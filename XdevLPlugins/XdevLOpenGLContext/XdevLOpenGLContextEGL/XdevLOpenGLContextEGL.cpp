0ea4b6bbeee5a2b8be78eacced33b300551ebc00
yaakuro <yaakuro@codeposer.net>
2015-12-04 19:38:38 +0900

    Add EGL plugin

---
+++ XdevLPlugins/XdevLOpenGLContext/XdevLOpenGLContextEGL/XdevLOpenGLContextEGL.cpp
@@ -0,0 +1,178 @@
+#include "XdevLOpenGLContextEGL.h"
+
+xdl::XdevLPluginDescriptor pluginDescriptor {
+	xdl::pluginName,
+	xdl::moduleNames,
+	XDEVLOPENGLCONTEXTEGL_PLUGIN_MAJOR_VERSION,
+	XDEVLOPENGLCONTEXTEGL_PLUGIN_MINOR_VERSION,
+	XDEVLOPENGLCONTEXTEGL_PLUGIN_PATCH_VERSION
+};
+
+xdl::XdevLModuleDescriptor moduleDescriptor {
+	xdl::vendor,
+	xdl::author,
+	xdl::moduleNames[0],
+	xdl::copyright,
+	xdl::moduleDescription,
+	XDEVLOPENGLCONTEXTEGL_MODULE_MAJOR_VERSION,
+	XDEVLOPENGLCONTEXTEGL_MODULE_MINOR_VERSION,
+	XDEVLOPENGLCONTEXTEGL_MODULE_PATCH_VERSION
+};
+
+extern "C" XDEVL_EXPORT xdl::xdl_int _create(xdl::XdevLModuleCreateParameter* parameter) {
+	if(moduleDescriptor.getName() == parameter->getModuleName()) {
+		xdl::XdevLModule* obj  = new xdl::XdevLOpenGLContextEGL(parameter);
+		if(!obj)
+			return xdl::ERR_ERROR;
+		parameter->setModuleInstance(obj);
+		return xdl::ERR_OK;
+	}
+
+	return xdl::ERR_MODULE_NOT_FOUND;
+}
+
+extern "C" XDEVL_EXPORT void _delete(xdl::XdevLModule* obj) {
+	if(obj)
+		delete obj;
+}
+
+extern "C" XDEVL_EXPORT xdl::XdevLPluginDescriptor* _getDescriptor() {
+	return &pluginDescriptor;
+}
+
+namespace xdl {
+
+		XdevLOpenGLContextEGL::XdevLOpenGLContextEGL(XdevLModuleCreateParameter* parameter) :
+			XdevLOpenGLContextBase(parameter, moduleDescriptor) {
+		}
+
+		XdevLOpenGLContextEGL::~XdevLOpenGLContextEGL() {
+
+		}
+
+		xdl_int XdevLOpenGLContextEGL::create(XdevLWindow* window) {
+
+			static const EGLint context_attribs[] = {
+				EGL_CONTEXT_CLIENT_VERSION, 2,
+				EGL_NONE
+			};
+			const char *extensions;
+
+			EGLint config_attribs[] = {
+				EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
+				EGL_RED_SIZE, 1,
+				EGL_GREEN_SIZE, 1,
+				EGL_BLUE_SIZE, 1,
+				EGL_ALPHA_SIZE, 1,
+				EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
+				EGL_NONE
+			};
+
+			EGLint major, minor, n, count, i, size;
+
+
+			EGLNativeDisplayType nativeDisplay;
+			EGLNativeWindowType nativeWindow;
+
+#if XDEVL_PLATFORM_WINDOWS
+			HINSTANCE	hInstance = static_cast<HWND>(m_window->getInternal(XdevLInternalName("WIN32_INSTANCE")));
+			if(nullptr == hInstance) {
+				XDEVL_MODULE_ERROR("Could not get native WIN32_INSTANCE information.\n");
+				return ERR_ERROR;
+			}
+
+			HWND	hWnd = static_cast<HWND>(m_window->getInternal(XdevLInternalName("WIN32_HWND")));
+			if(nullptr == hWnd) {
+				XDEVL_MODULE_ERROR("Could not get native WIN32_HWND information.\n");
+				return ERR_ERROR;
+			}
+			HDC hDC = static_cast<HWND>(m_window->getInternal(XdevLInternalName("WIN32_DC")));
+			if(nullptr == hDC) {
+				XDEVL_MODULE_ERROR("Could not get native WIN32_DC information.\n");
+				return ERR_ERROR;
+			}
+			nativeDisplay = (EGLNativeDisplayType)hDC;
+			nativeWindow = (EGLNativeWindowType)hWnd;
+#endif
+
+
+			m_eglDisplay = eglGetDisplay(nativeDisplay);
+			if(m_eglDisplay == EGL_NO_DISPLAY) {
+				XDEVL_MODULE_ERROR("Can't create egl display\n");
+				return ERR_ERROR;
+			} else {
+				XDEVL_MODULE_SUCCESS("Created egl display\n");
+			}
+
+			EGLBoolean ret = eglInitialize(m_eglDisplay, &major, &minor);
+			if(ret != EGL_TRUE) {
+				XDEVL_MODULE_ERROR("Can't initialize egl display\n");
+				return ERR_ERROR;
+			}
+
+			ret = eglBindAPI(EGL_OPENGL_ES_API);
+			if(ret != EGL_TRUE) {
+				XDEVL_MODULE_ERROR("Can't bind egl API to EGL_OPENGL_ES_API\n");
+				return ERR_ERROR;
+			}
+
+			if(!eglGetConfigs(m_eglDisplay, nullptr, 0, &count) || count < 1) {
+				return ERR_ERROR;
+			}
+
+			EGLConfig* configs = new EGLConfig[count];
+			ret = eglChooseConfig(m_eglDisplay, config_attribs, configs, count, &n);
+			if(ret != EGL_TRUE) {
+				return ERR_ERROR;
+			}
+
+			// TODO This is at the moment for only test purpose. As you can see only the first
+			// config is used.
+			for(i = 0; i < n; i++) {
+				eglGetConfigAttrib(m_eglDisplay, configs[i], EGL_BUFFER_SIZE, &size);
+				printf("Buffer size for config %d is %d\n", i, size);
+				// For now just use the first one.
+				m_eglConfig = configs[0];
+
+			}
+			delete [] configs;
+
+			// If we do not found any configs stop here.
+			if(m_eglConfig == nullptr) {
+				return ERR_ERROR;
+			}
+			m_eglContext = eglCreateContext(m_eglDisplay, m_eglConfig, EGL_NO_CONTEXT, context_attribs);
+			if(m_eglContext == nullptr) {
+				return ERR_ERROR;
+			}
+
+			m_eglSurface = eglCreateWindowSurface(display, config, nativeWindow, NULL);
+			if(m_eglSurface == nullptr) {
+				return ERR_ERROR;
+			}
+			return ERR_OK;
+		}
+		xdl_int XdevLOpenGLContextEGL::getAttributes(XdevLOpenGLContextAttributes& attributes) {
+			attributes = m_attributes;
+			return ERR_OK;
+		}
+		xdl_int XdevLOpenGLContextEGL::setAttributes(const XdevLOpenGLContextAttributes& attributes) {
+			m_attributes = attributes;
+			return ERR_OK;
+		}
+		xdl_int XdevLOpenGLContextEGL::makeCurrent(XdevLWindow* window) {
+			eglMakeCurrent(m_eglDisplay, m_eglSurface, m_eglSurface, m_eglContext);
+			return ERR_ERROR;
+		}
+		xdl_int XdevLOpenGLContextEGL::swapBuffers() {
+			eglSwapBuffers(m_eglDisplay, m_eglSurface);
+			return ERR_ERROR;
+		}
+		void* XdevLOpenGLContextEGL::getProcAddress(const xdl_char* func) {
+			return (void*)eglGetProcAddress(func);
+		}
+		xdl_int XdevLOpenGLContextEGL::setVSync(xdl_bool enableVSync) {
+			return ERR_ERROR;
+		}
+
+}
---
+++ XdevLPlugins/XdevLOpenGLContext/XdevLOpenGLContextEGL/XdevLOpenGLContextEGL.h
@@ -0,0 +1,64 @@
+#ifndef XDEVL_OPENGLCONTEXTEGL_IMPL_H
+#define XDEVL_OPENGLCONTEXTEGL_IMPL_H
+
+#include <XdevLPluginImpl.h>
+#include <XdevLOpenGLContext/XdevLOpenGLContext.h>
+#include <XdevLOpenGLContext/XdevLOpenGLContextBase.h>
+
+#include <XdevLPluginImpl.h>
+
+#include <GLES2/gl2.h>
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+
+namespace xdl {
+
+	static const XdevLString vendor {
+		"www.codeposer.net"
+	};
+
+	static const XdevLString author {
+		"Cengiz Terzibas"
+	};
+
+	static const XdevLString copyright {
+		"(c) 2005 - 2015 Cengiz Terzibas."
+	};
+
+	static const XdevLString pluginName {
+		"XdevLOpenGLContextEGL"
+	};
+
+	static const XdevLString moduleDescription {
+		"Module to create a EGL OpenGL context."
+	};
+
+	static std::vector<XdevLModuleName>	moduleNames {
+		XdevLModuleName("XdevLOpenGLContext"),
+	};
+
+	class XdevLOpenGLContextEGL : public XdevLOpenGLContextBase {
+		public:
+
+			XdevLOpenGLContextEGL(XdevLModuleCreateParameter* parameter);
+
+			virtual ~XdevLOpenGLContextEGL();
+			virtual xdl_int create(XdevLWindow* window) override;
+			virtual xdl_int getAttributes(XdevLOpenGLContextAttributes& attributes) override;
+			virtual xdl_int setAttributes(const XdevLOpenGLContextAttributes& attributes) override;
+			virtual xdl_int makeCurrent(XdevLWindow* window) override;
+			virtual xdl_int swapBuffers() override;
+			virtual void* getProcAddress(const xdl_char* func) override;
+			virtual xdl_int setVSync(xdl_bool enableVSync) override;
+
+		private:
+			EGLDisplay 			m_eglDisplay;
+			EGLSurface 			m_eglSurface;
+			EGLContext 			m_eglContext;
+			EGLConfig  			m_eglConfig;
+	};
+
+
+}
+
+#endif
