ef20a47dbe66755f76c06021940a5e15558ab9cd
yaakuro <yaakuro@codeposer.net>
2015-12-04 14:33:55 +0900

    Add support for multiple FTDI devices

--- XdevLPlugins/XdevLFTDI/XdevLFTDI.cpp
+++ XdevLPlugins/XdevLFTDI/XdevLFTDI.cpp
@@ -77,13 +77,17 @@ namespace xdl {
 
 	DWORD wrapXdevLParityToFTDI(XdevLSerialParity parity) {
 		switch(parity) {
-			case SERIAL_EVEN_PARITY: return FT_PARITY_EVEN;
+			case SERIAL_EVEN_PARITY:
+				return FT_PARITY_EVEN;
 				break;
-			case SERIAL_ODD_PARITY: return FT_PARITY_ODD;
+			case SERIAL_ODD_PARITY:
+				return FT_PARITY_ODD;
 				break;
-			case SERIAL_SPACE_PARITY: return FT_PARITY_SPACE;
+			case SERIAL_SPACE_PARITY:
+				return FT_PARITY_SPACE;
 				break;
-			case SERIAL_MARK_PARITY: return FT_PARITY_MARK;
+			case SERIAL_MARK_PARITY:
+				return FT_PARITY_MARK;
 			case SERIAL_NO_PARITY:
 			default:
 				break;
@@ -93,9 +97,11 @@ namespace xdl {
 
 	DWORD wrapXdevLFlowControlToFTDI(XdevLSerialFlowControl flowcontrol) {
 		switch(flowcontrol) {
-			case SERIAL_FLOW_CONTROL_HARDWARE: return FT_FLOW_RTS_CTS;
+			case SERIAL_FLOW_CONTROL_HARDWARE:
+				return FT_FLOW_RTS_CTS;
 				break;
-			case SERIAL_FLOW_CONTROL_SOFTWARE: return FT_FLOW_XON_XOFF;
+			case SERIAL_FLOW_CONTROL_SOFTWARE:
+				return FT_FLOW_XON_XOFF;
 				break;
 			case SERIAL_FLOW_CONTROL_NONE:
 			default:
@@ -106,9 +112,9 @@ namespace xdl {
 
 	XdevLFTDI::XdevLFTDI(XdevLModuleCreateParameter* parameter) :
 		XdevLModuleImpl<XdevLSerial>(parameter, moduleDescriptor),
-		m_usb_in_size(64),
-		m_usb_out_size(64),
-		m_latency_timer(16) {
+		m_usbInSize(2048),
+		m_usbOutSize(2048),
+		m_latencyTimer(16) {
 	};
 
 	XdevLFTDI::~XdevLFTDI() {
@@ -148,15 +154,15 @@ namespace xdl {
 					}
 					if(root->Attribute("usb_in_size")) {
 						std::istringstream ss(root->Attribute("usb_in_size"));
-						ss >> m_usb_in_size;
+						ss >> m_usbInSize;
 					}
 					if(root->Attribute("usb_out_size")) {
 						std::istringstream ss(root->Attribute("usb_out_size"));
-						ss >> m_usb_out_size;
+						ss >> m_usbOutSize;
 					}
 					if(root->Attribute("latency_timer")) {
 						std::istringstream ss(root->Attribute("latency_timer"));
-						ss >> m_latency_timer;
+						ss >> m_latencyTimer;
 					}
 					if(root->Attribute("baudrate")) {
 						std::istringstream ss(root->Attribute("baudrate"));
@@ -193,7 +199,7 @@ namespace xdl {
 		if(readInfoFromXMLFile() != ERR_OK) {
 			XDEVL_MODULE_WARNING("Parsing problems occurred of the Core XML file." << std::endl);
 		}
-		return _open();
+		return openConnection();
 	}
 
 	xdl::xdl_int XdevLFTDI::open(const XdevLFileName& name) {
@@ -203,7 +209,7 @@ namespace xdl {
 			XDEVL_MODULE_WARNING("Parsing problems occurred of the Core XML file." << std::endl);
 		}
 		m_deviceName = name;
-		return _open();
+		return openConnection();
 	}
 
 	xdl::xdl_int XdevLFTDI::open(const XdevLFileName& name, const XdevLDeviceModes& mode) {
@@ -213,21 +219,14 @@ namespace xdl {
 		}
 
 		m_deviceName = name;
-		return _open();
+		return openConnection();
 	}
 
-	xdl::xdl_int XdevLFTDI::_open() {
-
-		XDEVL_MODULE_INFO("Opening connection to the Serial Port device." << std::endl);
+	xdl::xdl_int XdevLFTDI::openConnection() {
+		XDEVL_MODULE_INFO("Checking for FTDI devices." << currentDevice.serial << std::endl);
 
 		FT_STATUS ftStatus;
 		DWORD iNumDevs = 0;
-		DWORD lpdwFlags[128];
-		DWORD lpdwType[128];
-		DWORD lpdwID[128];
-		DWORD lpdwLocId[128];
-		CHAR serial[128];
-		CHAR desc[128];
 
 		// Lets get the number of all connected devices.
 		ftStatus = FT_CreateDeviceInfoList(&iNumDevs);
@@ -238,27 +237,51 @@ namespace xdl {
 
 		// Now, lets get information about those connected devices.
 		for(xdl_uint i = 0; i < iNumDevs; i++) {
-			ftStatus = FT_GetDeviceInfoDetail(i, lpdwFlags, lpdwType, lpdwID, lpdwLocId, serial, desc, &ftHandle);
-			XDEVL_MODULE_INFO("------- Device " << &serial[0] << "-------" << std::endl);
-			XDEVL_MODULE_INFO("Name          : " << &desc[0] << std::endl);
-			XDEVL_MODULE_INFO("Type          : " << &lpdwType[0] << std::endl);
-			XDEVL_MODULE_INFO("ID            : " << &lpdwID[0] << std::endl);
+			XdevLFTDIDeviceInfo deviceInfo;
+			ftStatus = FT_GetDeviceInfoDetail(i, deviceInfo.flags, deviceInfo.type, deviceInfo.id, deviceInfo.locId, deviceInfo.serial, deviceInfo.desc, &deviceInfo.handle);
+			XDEVL_MODULE_INFO("--------------\n");
+			XDEVL_MODULE_INFO("Found device : " << deviceInfo.serial << " (Serial Number)" << std::endl);
+			XDEVL_MODULE_INFO("Name         : " << deviceInfo.desc << std::endl);
+			XDEVL_MODULE_INFO("--------------\n");
+
+			m_foundDevices.push_back(deviceInfo);
 		}
 		XDEVL_MODULE_INFO("------------------------------------------------------------\n");
 
 		//
-		// Open connection to the device
+		// Go through all devices and check if we find the one the user specified
 		//
-		ftStatus = FT_OpenEx(serial, FT_OPEN_BY_SERIAL_NUMBER, &ftHandle);
-		if(ftStatus != FT_OK) {
-			XDEVL_MODULE_ERROR("This can fail if the ftdi_sio driver is loaded use lsmod to check this and rmmod ftdi_sio to remove also rmmod usbserial");
+		xdl_bool deviceFound = xdl_false;
+		for(auto& device : m_foundDevices) {
+			if(strcmp(m_deviceName.toString().c_str(), (const xdl_char*)device.serial) == 0) {
+				deviceFound = xdl_true;
+				currentDevice = device;
+				currentDevice.usbInSize = m_usbInSize;
+				currentDevice.usbOutSize = m_usbOutSize;
+			}
+		}
+		if(xdl_false == deviceFound) {
+			XDEVL_MODULE_ERROR("Specified device: " << m_deviceName << " not found. For FTDI devices use the serial number you can see above.\n");
 			return ERR_ERROR;
 		}
 
+		XDEVL_MODULE_INFO("Opening connection to the Serial Port device: " << currentDevice.serial << std::endl);
+
+		//
+		// Open connection to the device
+		//
+		ftStatus = FT_OpenEx(currentDevice.serial, FT_OPEN_BY_SERIAL_NUMBER, &currentDevice.handle);
+		if(ftStatus != FT_OK) {
+			XDEVL_MODULE_ERROR("This can fail if the ftdi_sio driver is loaded use lsmod to check this and rmmod ftdi_sio to remove also rmmod usbserial");
+			XDEVL_MODULE_ERROR("Or specified device: " << m_deviceName << " not found. For FTDI devices use the serial number you can see above.\n");
+			return ERR_ERROR;
+		}
+		XDEVL_MODULE_INFO("Connection opened successfully." << std::endl);
+		
 		//
 		// Reset device
 		//
-		ftStatus = FT_ResetDevice(ftHandle);
+		ftStatus = FT_ResetDevice(currentDevice.handle);
 		if (ftStatus != FT_OK) {
 			XDEVL_MODULE_ERROR("FT_ResetDevice failed: " << (xdl_int)ftStatus << std::endl);
 			return ERR_ERROR;
@@ -269,7 +292,7 @@ namespace xdl {
 
 
 	xdl_int XdevLFTDI::setStates(xdl_int baudrate, XdevLSerialByteSize bytesize, XdevLSerialParity parity, XdevLSerialStopBits stopbits, XdevLSerialFlowControl flowcontrol, xdl_int timeout) {
-		XDEVL_MODULE_INFO("Setting Serial Port Protocol values." << std::endl)
+		XDEVL_MODULE_INFO("Setting Serial Port connection attributes." << std::endl)
 
 		// Set the internal values.
 		XdevLSerialImpl::setStates(baudrate, bytesize, parity, stopbits, flowcontrol, timeout);
@@ -279,7 +302,7 @@ namespace xdl {
 		//
 		// Set the baudrate
 		//
-		if((ftStatus = FT_SetBaudRate(ftHandle, baudrate)) != FT_OK) {
+		if((ftStatus = FT_SetBaudRate(currentDevice.handle, baudrate)) != FT_OK) {
 			XDEVL_MODULE_ERROR("FT_SetBaudRate failed: " << (xdl_int)ftStatus << "\n");
 			return ERR_ERROR;
 		}
@@ -302,11 +325,12 @@ namespace xdl {
 		//
 		// Set byte size, stop bits and parity.
 		//
-		ftStatus = FT_SetDataCharacteristics(ftHandle, bsize, sbits, sparity);
+		ftStatus = FT_SetDataCharacteristics(currentDevice.handle, bsize, sbits, sparity);
 		if(ftStatus != FT_OK) {
 			XDEVL_MODULE_ERROR("FT_SetDataCharacteristics failed: " << (xdl_int)ftStatus << "\n");
 			return ERR_ERROR;
 		}
+		XDEVL_MODULE_INFO("--------------\n");
 		XDEVL_MODULE_INFO("Baudrate           : " << baudrate << std::endl);
 		XDEVL_MODULE_INFO("Stop bits          : " << xdevLStopBitsToString(stopbits) << std::endl);
 		XDEVL_MODULE_INFO("Byte size          : " << xdevLByteSizeToString(bytesize) << std::endl);
@@ -317,7 +341,7 @@ namespace xdl {
 		//
 		DWORD fc = wrapXdevLFlowControlToFTDI(flowcontrol);
 
-		ftStatus = FT_SetFlowControl(ftHandle, fc, 0, 0);
+		ftStatus = FT_SetFlowControl(currentDevice.handle, fc, 0, 0);
 		if(ftStatus != FT_OK) {
 			XDEVL_MODULE_ERROR("FT_SetFlowControl failed: " << (xdl_int)ftStatus << "\n");
 			return ERR_ERROR;
@@ -327,28 +351,28 @@ namespace xdl {
 		//
 		// Set USB buffer parameters. (IN/OUT buffer size)
 		//
-		if((ftStatus = FT_SetUSBParameters(ftHandle, m_usb_in_size, m_usb_out_size)) != FT_OK) {
+		if((ftStatus = FT_SetUSBParameters(currentDevice.handle, currentDevice.usbInSize, currentDevice.usbOutSize)) != FT_OK) {
 			XDEVL_MODULE_ERROR("FT_SetUSBParameters failed: " << (xdl_int)ftStatus << "\n");
 			return ERR_ERROR;
 		}
-		XDEVL_MODULE_INFO("USB transfer size  : IN: " << m_usb_in_size << ": OUT: " << m_usb_out_size << " bytes." << std::endl);
+		XDEVL_MODULE_INFO("USB transfer size  : IN: " << currentDevice.usbInSize << ": OUT: " << currentDevice.usbOutSize << " bytes." << std::endl);
 
 
 		//
 		// Set the latency timer for better latency :D
 		//
-		if((ftStatus = FT_SetLatencyTimer(ftHandle, m_latency_timer)) != FT_OK) {
+		if((ftStatus = FT_SetLatencyTimer(currentDevice.handle, m_latencyTimer)) != FT_OK) {
 			XDEVL_MODULE_ERROR("FT_SetLatencyTimer failed: " << (xdl_int)ftStatus << "\n");
 			return ERR_ERROR;
 		}
-		XDEVL_MODULE_INFO("Latency timer      : " << m_latency_timer << " ms" << std::endl);
-
+		XDEVL_MODULE_INFO("Latency timer      : " << m_latencyTimer << " ms" << std::endl);
+		XDEVL_MODULE_INFO("--------------\n");
 
 		//
 		// Set the time out times for reading and writing.
 		//
 		if(timeout != 0) {
-			ftStatus = FT_SetTimeouts(ftHandle, m_timeout, m_timeout/1000000);
+			ftStatus = FT_SetTimeouts(currentDevice.handle, m_timeout, m_timeout/1000000);
 			if(ftStatus != FT_OK) {
 				XDEVL_MODULE_ERROR("Could not set the timeout.\n");
 			} else {
@@ -364,8 +388,8 @@ namespace xdl {
 		XDEVL_MODULE_INFO("Closing connection to Serial Port: '" << m_deviceName << "'" << std::endl);
 
 		// Check if we have a valid handle.
-		if(ftHandle != nullptr) {
-			FT_Close(ftHandle);
+		if(currentDevice.handle != nullptr) {
+			FT_Close(currentDevice.handle);
 		}
 
 		XDEVL_MODULE_SUCCESS("Connection to Serial Port closed successful." << std::endl);
@@ -373,10 +397,10 @@ namespace xdl {
 	}
 
 	xdl_int XdevLFTDI::write(xdl_uint8* src, xdl_int size) {
-		XDEVL_ASSERT(ftHandle, "It seems like the device wasn't created successfully.");
+		XDEVL_ASSERT(currentDevice.handle, "It seems like the device wasn't created successfully.");
 
 		DWORD 	dwBytesWrote;
-		FT_STATUS ftStatus = FT_Write(ftHandle, src, size, &dwBytesWrote);
+		FT_STATUS ftStatus = FT_Write(currentDevice.handle, src, size, &dwBytesWrote);
 		if(ftStatus != FT_OK) {
 			XDEVL_MODULE_ERROR("FT_Write error: " << ftStatus << "\n");
 			return -1;
@@ -386,10 +410,10 @@ namespace xdl {
 	}
 
 	xdl_int XdevLFTDI::read(xdl_uint8* dst, xdl_int size) {
-		XDEVL_ASSERT(ftHandle, "It seems like the device wasn't created successfully.");
+		XDEVL_ASSERT(currentDevice.handle, "It seems like the device wasn't created successfully.");
 
 		DWORD 	dwBytesRead;
-		FT_STATUS ftStatus = FT_Read(ftHandle, dst, size, &dwBytesRead);
+		FT_STATUS ftStatus = FT_Read(currentDevice.handle, dst, size, &dwBytesRead);
 		if(ftStatus != FT_OK) {
 			XDEVL_MODULE_ERROR("FT_Read error: " << ftStatus << "\n");
 			return -1;
@@ -400,10 +424,10 @@ namespace xdl {
 	}
 
 	xdl_int XdevLFTDI::waiting() {
-		XDEVL_ASSERT(ftHandle, "It seems like the device wasn't created successfully.");
+		XDEVL_ASSERT(currentDevice.handle, "It seems like the device wasn't created successfully.");
 
 		DWORD RxBytes;
-		FT_STATUS ftStatus = FT_GetQueueStatus(ftHandle,&RxBytes);
+		FT_STATUS ftStatus = FT_GetQueueStatus(currentDevice.handle,&RxBytes);
 		if(ftStatus != FT_OK) {
 			XDEVL_MODULE_ERROR("FT_GetQueueStatus error: " << ftStatus << "\n");
 			return -1;
@@ -413,9 +437,9 @@ namespace xdl {
 	}
 
 	xdl_int XdevLFTDI::flush() {
-		XDEVL_ASSERT(ftHandle, "It seems like the device wasn't created successfully.");
+		XDEVL_ASSERT(currentDevice.handle, "It seems like the device wasn't created successfully.");
 
-		FT_STATUS ftStatus = FT_Purge(ftHandle, FT_PURGE_RX | FT_PURGE_TX);
+		FT_STATUS ftStatus = FT_Purge(currentDevice.handle, FT_PURGE_RX | FT_PURGE_TX);
 		if(ftStatus != FT_OK) {
 			return ERR_ERROR;
 		}
