89f2321d3e2f8b8167d8aac8c1f843b157b3909d
yaakuro <yaakuro@codeposer.net>
2016-01-14 17:00:56 +0900

    Add new static build

--- XdevLPlugins/XdevLOpenGLContext/XdevLOpenGLContextGLX/XdevLOpenGLContextGLX.cpp
+++ XdevLPlugins/XdevLOpenGLContext/XdevLOpenGLContextGLX/XdevLOpenGLContextGLX.cpp
@@ -33,37 +33,6 @@
 #include "XdevLOpenGLContextGLX.h"
 #include <tinyxml.h>
 
-
-
-xdl::XdevLModuleDescriptor moduleDescriptor {
-	xdl::XdevLString("www.codeposer.net"),
-	xdl::XdevLString("Cengiz Terzibas"),
-	xdl::moduleNames[0],
-	xdl::XdevLString("(c) 2005 - 2016 Cengiz Terzibas."),
-	xdl::XdevLString("Module to create a GLX OpenGL context."),
-	XDEVLOPENGLCONTEXT_MODULE_MAJOR_VERSION,
-	XDEVLOPENGLCONTEXT_MODULE_MINOR_VERSION,
-	XDEVLOPENGLCONTEXT_MODULE_PATCH_VERSION
-};
-
-xdl::XdevLPluginDescriptor glxPluginDescriptor {
-	xdl::XdevLString("XdevLOpenGLContextCGL"),
-	xdl::moduleNames,
-	XDEVLOPENGLCONTEXT_MAJOR_VERSION,
-	XDEVLOPENGLCONTEXT_MINOR_VERSION,
-	XDEVLOPENGLCONTEXT_PATCH_VERSION
-};
-
-XDEVL_PLUGIN_INIT_DEFAULT
-XDEVL_PLUGIN_SHUTDOWN_DEFAULT
-XDEVL_PLUGIN_DELETE_MODULE_DEFAULT
-XDEVL_PLUGIN_GET_DESCRIPTOR_DEFAULT(glxPluginDescriptor);
-
-XDEVL_PLUGIN_CREATE_MODULE {
-	XDEVL_PLUGIN_CREATE_MODULE_INSTANCE(xdl::XdevLOpenGLContextGLX, moduleDescriptor)
-	XDEVL_PLUGIN_CREATE_MODULE_NOT_FOUND
-}
-
 namespace xdl {
 
 	static xdl_bool contextErrorOccured = xdl_false;
