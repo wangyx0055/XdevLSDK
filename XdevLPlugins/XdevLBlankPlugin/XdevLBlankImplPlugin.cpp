8dc11821932a32e6c53f33bc133916a6f3b7e321
yaakuro <yaakuro@codeposer.net>
2016-01-14 15:37:51 +0900

    Add Satic linking

---
+++ XdevLPlugins/XdevLBlankPlugin/XdevLBlankImplPlugin.cpp
@@ -0,0 +1,30 @@
+#include "XdevLBlankImpl.h"
+
+xdl::XdevLPluginDescriptor blankPluginDescriptor {
+	xdl::XdevLString("XdevLBlank"),
+	xdl::moduleNames,
+	XDEVLBLANK_MAJOR_VERSION,
+	XDEVLBLANK_MINOR_VERSION,
+	XDEVLBLANK_PATCH_VERSION
+};
+
+xdl::XdevLModuleDescriptor blankModuleDescriptor {
+	xdl::XdevLString("www.codeposer.net"),
+	xdl::XdevLString("Cengiz Terzibas"),
+	xdl::moduleNames[0],
+	xdl::XdevLString("(c) 2005 - 2016 Cengiz Terzibas."),
+	xdl::XdevLString("This is a description of an empty module"),
+	XDEVLBLANK_MODULE_MAJOR_VERSION,
+	XDEVLBLANK_MODULE_MINOR_VERSION,
+	XDEVLBLANK_MODULE_PATCH_VERSION
+};
+
+XDEVL_PLUGIN_INIT_DEFAULT
+XDEVL_PLUGIN_SHUTDOWN_DEFAULT
+XDEVL_PLUGIN_DELETE_MODULE_DEFAULT
+XDEVL_PLUGIN_GET_DESCRIPTOR_DEFAULT(blankPluginDescriptor);
+
+XDEVL_PLUGIN_CREATE_MODULE {
+	XDEVL_PLUGIN_CREATE_MODULE_INSTANCE(xdl::XdevLBlankModuleImpl, blankModuleDescriptor)
+	XDEVL_PLUGIN_CREATE_MODULE_NOT_FOUND
+}
