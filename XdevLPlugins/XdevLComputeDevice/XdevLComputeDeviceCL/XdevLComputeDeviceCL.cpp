de9fd47972ec35d47dee6bd5ec0b996195dbf935
Yaakuro <yaakuro@codeposer.net>
2016-01-25 22:04:15 +0900

    Add some code

--- XdevLPlugins/XdevLComputeDevice/XdevLComputeDeviceCL/XdevLComputeDeviceCL.cpp
+++ XdevLPlugins/XdevLComputeDevice/XdevLComputeDeviceCL/XdevLComputeDeviceCL.cpp
@@ -1,21 +1,21 @@
 /*
 	Copyright (c) 2005 - 2016 Cengiz Terzibas
 
-	Permission is hereby granted, free of charge, to any person obtaining a copy of 
-	this software and associated documentation files (the "Software"), to deal in the 
-	Software without restriction, including without limitation the rights to use, copy, 
-	modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
-	and to permit persons to whom the Software is furnished to do so, subject to the 
+	Permission is hereby granted, free of charge, to any person obtaining a copy of
+	this software and associated documentation files (the "Software"), to deal in the
+	Software without restriction, including without limitation the rights to use, copy,
+	modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+	and to permit persons to whom the Software is furnished to do so, subject to the
 	following conditions:
 
-	The above copyright notice and this permission notice shall be included in all copies 
+	The above copyright notice and this permission notice shall be included in all copies
 	or substantial portions of the Software.
 
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
-	INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
-	PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
-	FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
-	OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+	INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+	PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+	FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+	OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 	DEALINGS IN THE SOFTWARE.
 
 	cengiz@terzibas.de
@@ -29,6 +29,37 @@
 
 namespace xdl {
 
+	XdevLComputeDeviceContextCL::XdevLComputeDeviceContextCL(cl_device_id id, cl_context ctx) :
+		m_deviceID(id),
+		m_context(ctx) {
+
+	}
+
+	XdevLComputeDeviceContextCL::~XdevLComputeDeviceContextCL() {
+		clReleaseContext(m_context);
+	}
+
+	XdevLComputeDeviceQueue* XdevLComputeDeviceContextCL::createCommandBuffer() {
+		cl_int ret;
+		cl_command_queue commandQueue = clCreateCommandQueue(m_context, m_deviceID, 0, &ret);
+		auto tmp = new XdevLComputeDeviceQueue(commandQueue);
+		return tmp;
+	}
+
+
+	cl_device_id XdevLComputeDeviceContextCL::getDeviceId() {
+		return m_deviceID;
+	}
+	cl_context XdevLComputeDeviceContextCL::getContext() {
+		return m_context;
+	}
+
+
+
+//
+//
+//
+
 
 	XdevLComputeDeviceCL::XdevLComputeDeviceCL(XdevLModuleCreateParameter* parameter, const XdevLModuleDescriptor& descriptor) :
 		XdevLModuleImpl<XdevLComputeDevice>(parameter, descriptor) {
@@ -38,5 +69,107 @@ namespace xdl {
 	XdevLComputeDeviceCL::~XdevLComputeDeviceCL() {
 		XDEVL_MODULE_INFO("~XdevLComputeDeviceCL()\n");
 	}
+
+	int XdevLComputeDeviceCL::init() {
+		cl_int ret;
+
+		cl_uint numPlatforms;
+		ret = clGetPlatformIDs(1, &m_platformID, &numPlatforms);
+		std::vector<cl_platform_id> platformIDs(numPlatforms);
+
+		ret = clGetPlatformIDs(1, platformIDs.data(), nullptr);
+
+		for(auto platform : platformIDs) {
+			XdevLPlatformInfo info;
+			info.id = platform;
+
+			std::size_t size;
+			ret = clGetPlatformInfo(platform, CL_PLATFORM_PROFILE, sizeof(info.profile), (void*)info.profile, &size);
+			ret = clGetPlatformInfo(platform, CL_PLATFORM_VERSION, sizeof(info.version), (void*)info.version, &size);
+			ret = clGetPlatformInfo(platform, CL_PLATFORM_NAME, sizeof(info.name), (void*)info.name, &size);
+			ret = clGetPlatformInfo(platform, CL_PLATFORM_VENDOR, sizeof(info.vendor), (void*)info.vendor, &size);
+
+			if(CL_SUCCESS != ret) {
+				return -1;
+			}
+
+			std::cout << "Platform Name       : " << info.name << std::endl;
+			std::cout << "Platform Profile    : " << info.profile << std::endl;
+			std::cout << "Platform Version    : " << info.version << std::endl;
+			std::cout << "Platform Vendor     : " << info.vendor << std::endl;
+
+
+			m_platforms.push_back(std::move(info));
+		}
+
+		for(auto platform : m_platforms) {
+			cl_uint numDevices;
+			ret = clGetDeviceIDs(platform.id, CL_DEVICE_TYPE_ALL, 1, &m_deviceID, &numDevices);
+			std::cout << "OpenCL Number of devices: " << numDevices << ", ID: " << m_deviceID << std::endl;
+
+			XdevLDeviceInfo device;
+			device.id = m_deviceID;
+
+			std::size_t size;
+			ret = clGetDeviceInfo(m_deviceID, CL_DEVICE_NAME, sizeof(device.name), (void*)device.name, &size);
+			ret = clGetDeviceInfo(m_deviceID, CL_DEVICE_VENDOR, sizeof(device.vendor), (void*)device.vendor, &size);
+			ret = clGetDeviceInfo(m_deviceID, CL_DRIVER_VERSION, sizeof(device.version), (void*)device.version, &size);
+			ret = clGetDeviceInfo(m_deviceID, CL_DEVICE_PROFILE, sizeof(device.profile), (void*)device.profile, &size);
+
+			if(CL_SUCCESS != ret) {
+				return -1;
+			}
+
+			std::cout << "Device Profile    : " << device.profile << std::endl;
+			std::cout << "Device Name       : " << device.name << std::endl;
+			std::cout << "Device Version    : " << device.version << std::endl;
+			std::cout << "Device Vendor     : " << device.vendor << std::endl;
+
+
+			m_devices.push_back(std::move(device));
+		}
+		return 0;
+	}
+
+	int XdevLComputeDeviceCL::shutdown() {
+
+		return 0;
+	}
+
+	XdevLComputeDeviceContext* XdevLComputeDeviceCL::createContext(const XdevLComputePlatformId platformId, const XdevLComputeDeviceId& deviceId) {
+		cl_int ret;
+		const cl_context_properties properties [] = {
+			CL_CONTEXT_PLATFORM,
+			reinterpret_cast<cl_context_properties>(m_platforms[platformId].id),
+			0,
+			0
+		};
+
+		cl_context ctx = clCreateContext(properties, 1, &m_devices[deviceId].id, nullptr, nullptr, &ret);
+		if(nullptr == ctx) {
+			return nullptr;
+		}
+
+		auto tmp = new XdevLComputeDeviceContext(m_devices[deviceId].id, ctx);
+		return tmp;
+	}
+
+	int main(int argc, char **argv) {
+		std::unique_ptr<XdevLComputeDeviceCL> cd= std::unique_ptr<XdevLComputeDeviceCL>(new XdevLComputeDeviceCL()) ;
+		cd->init();
+
+		auto ctx = cd->createContext();
+		if(nullptr == ctx) {
+			std::cerr << "Could not create context.\n";
+		}
+
+		auto queue = ctx->createCommandBuffer();
+
+
+		cd->shutdown();
+		return 0;
+	}
+
+
 
 }
--- XdevLPlugins/XdevLComputeDevice/XdevLComputeDeviceCL/XdevLComputeDeviceCL.h
+++ XdevLPlugins/XdevLComputeDevice/XdevLComputeDeviceCL/XdevLComputeDeviceCL.h
@@ -1,21 +1,21 @@
 /*
 	Copyright (c) 2005 - 2016 Cengiz Terzibas
 
-	Permission is hereby granted, free of charge, to any person obtaining a copy of 
-	this software and associated documentation files (the "Software"), to deal in the 
-	Software without restriction, including without limitation the rights to use, copy, 
-	modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
-	and to permit persons to whom the Software is furnished to do so, subject to the 
+	Permission is hereby granted, free of charge, to any person obtaining a copy of
+	this software and associated documentation files (the "Software"), to deal in the
+	Software without restriction, including without limitation the rights to use, copy,
+	modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+	and to permit persons to whom the Software is furnished to do so, subject to the
 	following conditions:
 
-	The above copyright notice and this permission notice shall be included in all copies 
+	The above copyright notice and this permission notice shall be included in all copies
 	or substantial portions of the Software.
 
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
-	INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
-	PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
-	FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
-	OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+	INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+	PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+	FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+	OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 	DEALINGS IN THE SOFTWARE.
 
 	cengiz@terzibas.de
@@ -24,20 +24,103 @@
 #ifndef XDEVL_COMPUTE_DEVICE_CL_MODULE_IMPL_H
 #define XDEVL_COMPUTE_DEVICE_CL_MODULE_IMPL_H
 
+
+#ifdef __APPLE__
+#include <OpenCL/opencl.h>
+#else
+#include <CL/cl.h>
+#endif
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <memory>
+
 #include <XdevLComputeDevice/XdevLComputeDevice.h>
 #include <XdevLPluginImpl.h>
 
 namespace xdl {
 
-	class XdevLComputeDeviceCL : public XdevLModuleImpl<XdevLComputeDevice> {
+
+
+	struct XdevLPlatformInfo {
+		cl_platform_id id;
+		char profile[128];
+		char version[128];
+		char name[128];
+		char vendor[128];
+	};
+
+	struct XdevLDeviceInfo {
+		cl_device_id id;
+		char profile[128];
+		char version[128];
+		char name[128];
+		char vendor[128];
+	};
+
+//
+//
+//
+
+	class XdevLComputeDeviceQueueCL : public XdevLComputeDeviceQueue {
 		public:
-			XdevLComputeDeviceCL(XdevLModuleCreateParameter* parameter, const XdevLModuleDescriptor& descriptor);
-			virtual ~XdevLComputeDeviceCL();
+			XdevLComputeDeviceQueueCL(cl_command_queue queue) : m_CommandQueue(queue) {}
+		private:
+			cl_command_queue m_CommandQueue;
+	};
 
+//
+//
+//
 
+	class XdevLComputeDeviceContextCL : public XdevLComputeDeviceContext {
+		public:
+			XdevLComputeDeviceContextCL(cl_device_id id, cl_context ctx);
+
+			virtual ~XdevLComputeDeviceContextCL();
+
+			XdevLComputeDeviceQueue* createCommandBuffer();
+
+			cl_device_id getDeviceId();
+			cl_context getContext();
+
+		private:
+			cl_device_id m_deviceID;
+			cl_context m_context;
 	};
 
 
+
+//
+//
+//
+
+	class XdevLComputeDeviceCL : public XdevLModuleImpl<XdevLComputeDeviceCL> {
+		public:
+			XdevLComputeDeviceCL(XdevLModuleCreateParameter* parameter, const XdevLModuleDescriptor& descriptor);
+			virtual ~XdevLComputeDeviceCL();
+			virtual int init();
+			virtual int shutdown();
+
+			virtual XdevLComputeDeviceContext* createContext(const XdevLComputePlatformId platformId = XDEVL_COMPUTE_PLATFORM_DEFAULT, const XdevLComputeDeviceId& deviceId = XDEVL_COMPUTE_DEVICE_DEFAULT);
+
+		private:
+			cl_platform_id m_platformID;
+			cl_device_id m_deviceID;
+			std::vector<XdevLPlatformInfo> m_platforms;
+			std::vector<XdevLDeviceInfo> m_devices;
+	};
+
+	XdevLComputeDeviceCL::XdevLComputeDeviceCL() :
+		m_platformID(nullptr),
+		m_deviceID(nullptr) {
+
+	}
+
+	XdevLComputeDeviceCL::~XdevLComputeDeviceCL() {
+
+	}
 
 }
 
