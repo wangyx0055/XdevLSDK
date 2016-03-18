c8f64e3fbfbbae4606173f1cdc551264721e29b0
yaakuro <yaakuro@codeposer.net>
2016-03-18 15:42:46 +0900

    Separate files again

---
+++ XdevL/include/XdevL/Unix/XdevLConditionalVariableUnix.h
@@ -0,0 +1,58 @@
+/*
+	XdevL eXtended DEVice Library.
+
+	Copyright © 2005-2016 Cengiz Terzibas
+
+	This library is free software; you can redistribute it and/or modify it under the
+	terms of the GNU Lesser General Public License as published by the Free Software
+	Foundation; either version 2.1 of the License, or (at your option) any later version.
+	This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+	without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+	See the GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License along with
+	this library; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+	Suite 330, Boston, MA 02111-1307 USA
+
+	I would appreciate if you report all bugs to: cengiz@terzibas.de
+*/
+
+#ifndef XDEVL_CONDITIONAL_VARIABLE_UNIX_H
+#define XDEVL_CONDITIONAL_VARIABLE_UNIX_H
+
+#include <XdevLTypes.h>
+#include <XdevLMutex.h>
+
+namespace thread {
+
+	/**
+	 @class ConditionalVariable
+	 @brief Conditional variable.
+	*/
+	class ConditionalVariable {
+		public:
+			/// Initialize the conditional variable.
+			ConditionalVariable();
+
+			/// Destroy the conditional variable.
+			~ConditionalVariable();
+
+			/// Send signal.
+			int signal();
+
+			/// Send signal to all.
+			int brodcast();
+
+			/// Wait for signal.
+			int wait(Mutex& mutex);
+
+		private:
+
+			pthread_cond_t m_condition;
+
+	};
+
+
+}
+
+#endif
---
+++ XdevL/src/Unix/XdevLConditionalVariableUnix.cpp
@@ -0,0 +1,72 @@
+/*
+	Copyright (c) 2005 - 2016 Cengiz Terzibas
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy of 
+	this software and associated documentation files (the "Software"), to deal in the 
+	Software without restriction, including without limitation the rights to use, copy, 
+	modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+	and to permit persons to whom the Software is furnished to do so, subject to the 
+	following conditions:
+
+	The above copyright notice and this permission notice shall be included in all copies 
+	or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+	INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+	PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+	FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+	OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+	DEALINGS IN THE SOFTWARE.
+
+	cengiz@terzibas.de
+*/
+
+#include <XdevLConditionalVariable.h>
+#include <iostream>
+#include <errno.h>
+#include <string.h>
+
+	namespace thread {
+
+		ConditionalVariable::ConditionalVariable() {
+			if ( pthread_cond_init(&m_condition, NULL) != 0 ) {
+				throw("Mutex::Could not create mutex");
+			}
+		}
+
+		ConditionalVariable::~ConditionalVariable() {
+
+		}
+
+		int ConditionalVariable::signal() {
+
+			if (pthread_cond_signal(&m_condition) != 0) {
+				std::cerr << "ConditionalVariable::Could not send signal: " << strerror(errno) << std::endl;
+				return 1;
+			}
+
+			return 0;
+		}
+
+		int ConditionalVariable::brodcast() {
+
+			if ( pthread_cond_broadcast(&m_condition) != 0) {
+				std::cerr << "ConditionalVariable::Could send signal to all: " << strerror(errno) << std::endl;
+				return 1;
+			}
+
+			return 0;
+		}
+
+		int ConditionalVariable::wait(Mutex& mutex) {
+
+			if (pthread_cond_wait(&m_condition, &mutex.getNativeMutex()) != 0) {
+				std::cerr << "ConditionalVariable::Could wait: " << strerror(errno) << std::endl;
+				return 1;
+			}
+
+			return 0;
+		}
+
+	
+}
